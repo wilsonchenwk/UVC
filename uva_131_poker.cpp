e9a6754cdd57de674aa2d329db23357c898baad7
Wilson <wilsonchen@weintek.com>
2022-06-14 00:01:33 +0800

    add 131 ok solution

--- uva_131_poker.cpp
+++ uva_131_poker.cpp
@@ -5,7 +5,7 @@
 #endif
 using namespace std;
 
-const std::vector<std::string> hand_name_table={
+const std::string hand_name_table[9]{
 "highest-card",
 "one-pair",
 "two-pairs",
@@ -29,184 +29,330 @@ four_a_kind,
 straight_flush
 };
 
-std::unordered_map<std::string, int> poker_num_map = {{"A", 1},{"2", 2},{"3", 3},{"4", 4},{"5", 5},{"6", 6},{"7", 7},\
+std::map<std::string, int> poker_num_map = {{"A", 1},{"2", 2},{"3", 3},{"4", 4},{"5", 5},{"6", 6},{"7", 7},\
                                             {"8", 8},{"9", 9},{"T", 10},{"J", 11},{"Q", 12},{"K", 13}};
-std::unordered_map<std::string, int> poker_suit_map = {{"C", 0},{"D", 1},{"H", 2},{"S", 3}};
+std::map<std::string, int> poker_suit_map = {{"C", 1},{"D", 2},{"H", 3},{"S", 4}};
 
 #define P_NUM_INT(str)   poker_num_map[str.substr(0,1)]
 #define P_NUM_STR(str)   str.substr(0,1)
 #define P_SUIT_INT(str)  poker_suit_map[str.substr(1,1)]
 #define P_SUIT_STR(str)  str.substr(1,1)
+class  Poker_Composite
+{
+public:
+Poker_Composite()
+{
+    num_map.resize(13+1+1);
+    suit_map.resize(4+1);
+}
+vector<uint8_t> num_map;
+vector<uint8_t> suit_map;
+
+}; 
+
 class Poker_131
 {
 public:
     Poker_131() //constructor
     {
         round_cards.resize(10);
-        init_done = false;
     }
     bool round_init(std::istream &is,std::ostream &os) // true:ok  false:fail
     {
         int i = 0;
         best_hand_num = hi_card;
-        check_done = false;
-        while(std::getline(is, round_cards[i],' '))
+        std::fill(hand_comp.num_map.begin(), hand_comp.num_map.end(), 0);
+        std::fill(hand_comp.suit_map.begin(), hand_comp.suit_map.end(), 0);
+        std::fill(deck_comp.num_map.begin(), deck_comp.num_map.end(), 0);
+        std::fill(deck_comp.suit_map.begin(), deck_comp.suit_map.end(), 0);
+        std::string line;
+        std::getline(is, line);
+        // if (line == "")
+        //     return false;
+        std::stringstream ss;
+        ss << line;
+        while(ss>> round_cards[i])
         {
-            if(++i == 9){
-            std::getline(is, round_cards[i++]);    
+            if(++i == 10)
                 break;
-            }
         }
-        if(i==10){
+        if(i!=10)
+        return false;
             os<<"Hand: ";    
             for(i=0;i<5;i++)
             {
-                os<<round_cards[i]<<" ";  
+                os<<round_cards[i]<<" ";
+                hand_comp.num_map[P_NUM_INT(round_cards[i])]++;
+                hand_comp.num_map[14] += (P_NUM_STR(round_cards[i]) =="A") * 1;
+                hand_comp.suit_map[P_SUIT_INT(round_cards[i])]++;
             }
             os<<"Deck: ";
             for(i=5;i<10;i++)
             {
-                os<<round_cards[i]<<" ";  
+                os<<round_cards[i]<<" ";
+                deck_comp.num_map[P_NUM_INT(round_cards[i])]++;
+                deck_comp.num_map[14] += (P_NUM_STR(round_cards[i]) =="A") * 1;
+                deck_comp.suit_map[P_SUIT_INT(round_cards[i])]++;  
             }
-
-            init_done = true;
-            check_done = false;
+  
             return true;
-        }
-        else{
-            init_done = false;   
-            return false;
-        }
+        
+
     }
     bool round_check(void) // true:ok  false:fail
     {
-        if(!init_done)
-            return false;
-        for(int i=5;i<=10 && best_hand_num!=straight_flush;i++)
+
+        HAND_NAME_ENUM tmp= find_best_hand(deck_comp);
+        best_hand_num = find_best_hand(hand_comp);
+        if(best_hand_num<tmp)
+            best_hand_num = tmp;    
+        std::vector<std::string> deck_cards;
+        for(int i=5;i<9 && best_hand_num!=straight_flush;i++)
         {
-            std::sort(round_cards.begin(), round_cards.begin()+i, [](const string &a, const string &b){
-                    return P_NUM_INT(a) < P_NUM_INT(b); // 升序排列
-            });
-            HAND_NAME_ENUM hand =  find_best_hand(i);
+            hand_comp.num_map[P_NUM_INT(round_cards[i])]++;
+            hand_comp.num_map[14] += (P_NUM_STR(round_cards[i]) =="A") * 1;
+            hand_comp.suit_map[P_SUIT_INT(round_cards[i])]++;
+            deck_cards.push_back(round_cards[i]);
+            HAND_NAME_ENUM hand =  find_best_hand(hand_comp,&deck_cards,best_hand_num);
             if(best_hand_num < hand)
                 best_hand_num = hand;
         }
-        check_done = true;
         return true;
     }
     bool result_output(std::ostream &os) // true:ok  false:fail
     {
-        if(!check_done)
-            return false;
+
         os<<"Best hand: "<<hand_name_table[best_hand_num]<<endl;    
         return true;
     }
 private:
+    Poker_Composite hand_comp,deck_comp;
     std::vector<std::string> round_cards;
     HAND_NAME_ENUM best_hand_num;
     bool init_done;
     bool check_done;
-    HAND_NAME_ENUM find_best_hand(int num) 
-    {
-        int conti_cnt = 1 , same_cnt = 1,sf_test=1;
-        int p=0,k3=0,k4=0,f=0;
-        int pre_diff_num = P_NUM_INT(round_cards[0]);
-        int  suit_cnt[4]={1};
-        bool  suit_on_flush_bit=0;
-        bool  suit_on_straight_bit=0;
-        for(int i =1;i<num;i++)
-        {
 
-            if(P_NUM_INT(round_cards[i]) == P_NUM_INT(round_cards[i-1]))
-                same_cnt++;
-            else
-            {
-                if(P_NUM_INT(round_cards[i]) == 1 + pre_diff_num)
-                    conti_cnt++;
-                else
+    HAND_NAME_ENUM find_best_hand(Poker_Composite &total, std::vector<std::string>  *must =nullptr,HAND_NAME_ENUM current = (HAND_NAME_ENUM)hi_card) 
+    {
+        uint16_t i,j;
+        int flush_idx=0,straight_begin=0,num_k4=0,k3_idx=0;
+        vector<uint8_t> v_pair,v_k3;
+        for(i=1;i<=14;i++)
+        {
+            if(total.num_map[i]==0)
+                continue;
+            else{
+                if(i<14)    
+                {    if(total.num_map[i]==2 )
+                        v_pair.push_back(i);
+                    else if(total.num_map[i]==3)
+                        k3_idx = i;
+                    else if(total.num_map[i]==4)
+                        num_k4 = i;
+                }
+                if(straight_begin==0 && i<=10 && total.num_map[i] && total.num_map[i+1] && total.num_map[i+2]
+                     && total.num_map[i+3] && total.num_map[i+4]){
+                     straight_begin =i;            
+                }
+            }
+
+        }
+        int straight_end = straight_begin +4;
+        while(straight_end < 14 && total.num_map[straight_end + 1])
+        {
+            straight_end++;
+        }
+        for(i=1;i<=4;i++)
+        {
+            if(total.suit_map[i]>=5)       
+                flush_idx = i;
+        }
+        switch(current+1)
+        {
+            case(one_pair): //not completed
+                if(v_pair.size()==1)
                 {
-                    if(conti_cnt == 4 && (P_NUM_STR(round_cards[0]) =="A" &&  P_NUM_STR(round_cards[i])=="K")){
-                        conti_cnt ++;        
+                    if(must!=nullptr)
+                    {
+                        for(j=0;j<v_pair.size();j++)
+                        {
+                            if(P_NUM_INT(must->at(must->size()-1)) == v_pair[j])
+                                current = one_pair; 
+                        }
+                        
                     }
                     else
-                        conti_cnt = 1;           
+                        current = one_pair;           
                 }
-                pre_diff_num = P_NUM_INT(round_cards[i]);
-                if(conti_cnt == 5)
+            case(two_pairs): //not completed
+                if(v_pair.size()==2)
                 {
-                    suit_on_straight_bit|= (1<<P_SUIT_INT(round_cards[i]));
-                    conti_cnt = 1;
+                    if(must!=nullptr)
+                    {
+                        int k=0;
+                        int num=must->size() -1;
+                        for(i=0;i<must->size();i++)
+                        {
+                                
+                                for(j=0;j<v_pair.size();j++)
+                                {
+                                    if(P_NUM_INT(must->at(i)) == v_pair[j]){
+                                        k++;
+                                        break;
+                                    }     
+                                }
+                        }
+                        if(k>=num)
+                             current = two_pairs;
+                    }
+                    else
+                        current = two_pairs;
                 }
-                
-                if(same_cnt==4){
-                    k4++;
+            case(three_a_kind): //not completed
+                if(k3_idx)
+                {   
+                    if(must!=nullptr)
+                    {
+                        int num=must->size() -2;
+                        if(P_NUM_INT(must->at(must->size() -1)) == k3_idx)
+                        {    
+                            int k=0;
+                            for(i=0;i<must->size();i++)
+                            {
+                                
+                                if(P_NUM_INT(must->at(i)) == k3_idx)
+                                    k++;
+                            }
+                            if(k>=num)
+                                current = three_a_kind; 
+                        }
+                    }
+                    else
+                        current = three_a_kind;   
                 }
-                else if(same_cnt==3){
-                    k3++;
+            case(straight):
+                if(straight_begin)
+                {   
+                    if(must!=nullptr)
+                    {
+                        for(i=0;i<must->size();i++)
+                        {
+                            if(P_NUM_INT(must->at(i)) > straight_end)       
+                                break;
+                            if(P_NUM_INT(must->at(i)) < straight_begin && !(straight_end==14 && P_NUM_INT(must->at(i))==1))       
+                                break;
+    
+                            if(i==must->size()-1)
+                                current = straight;   
+                        }
+                    }
+                    else
+                        current = straight;   
                 }
-                else if(same_cnt==2){
-                    p++;
+            case(_flush):
+                if(flush_idx)
+                {   
+                    if(must!=nullptr)
+                    {
+                        for(i=0;i<must->size();i++)
+                        {
+                            if(P_SUIT_INT(must->at(i)) != flush_idx)
+                                break;
+                            if(i==must->size()-1){
+                                current = _flush;
+                            }   
+                        }        
+                    }
+                    else
+                        current = _flush;   
                 }
-                same_cnt = 1;
-            }
-            if(P_SUIT_STR(round_cards[i]) ==  P_SUIT_STR(round_cards[i-1]))
-            {    
-                suit_cnt[P_SUIT_INT(round_cards[i])] ++;
-                
+            case(full_house):
+                if(k3_idx && v_pair.size())
+                {   
 
-            }
-               
-            if(suit_cnt[P_SUIT_INT(round_cards[i])] ==5)
-                suit_on_straight_bit|= (1<<P_SUIT_INT(round_cards[i]));
-            
-        }
-        if(conti_cnt == 4 && (P_NUM_STR(round_cards[0]) =="A" &&  P_NUM_STR(round_cards[9])=="K"))
-        {    
-            suit_on_straight_bit|= (1<<P_SUIT_INT(round_cards[0]));  
-        }
+                    if(must!=nullptr)
+                    {
+                        for(i=0;i<must->size();i++)
+                        {
+                            if(P_NUM_INT(must->at(i)) != k3_idx)       
+                            {
+                                int k=0;
+                                for(j=0;j<v_pair.size();j++)
+                                {
+                                    if(P_NUM_INT(must->at(i)) == v_pair[i])
+                                        k=1;
+                                }
+                                if(!k)
+                                    break;
+                            }
+                            if(i==must->size()-1)
+                                current = full_house;   
+                        }
+                    }
+                    else
+                        current = full_house ;           
                     
-        if((suit_on_flush_bit & suit_on_straight_bit) && is_straight_flush(num,suit_on_flush_bit & suit_on_straight_bit))
-            return straight_flush;
-        if(k4)
-            return four_a_kind;
-        if(k3 && p)
-            return full_house;
-        if(suit_on_flush_bit)
-            return _flush;
-        if(suit_on_straight_bit)
-            return straight;
-        if(k3)
-            return three_a_kind;
-        if(p>=2)
-            return two_pairs;
-        else if(p)
-            return one_pair;
-        else
-            return hi_card;              
+            
+                }
+            case(four_a_kind):
+                if(num_k4)
+                {   
+                    if(must!=nullptr)
+                    {
+                        int num=must->size() -1;
+                        if(P_NUM_INT(must->at(must->size() -1)) == num_k4)
+                        {    
+                            int k=0;
+                            for(i=0;i<must->size();i++)
+                            {
+                                
+                                if(P_NUM_INT(must->at(i)) == num_k4)
+                                    k++;
+                            }
+                            if(k>=num)
+                                current = four_a_kind; 
+                        }
+                    }
+                    else
+                        current = four_a_kind;   
+                }
+            case(straight_flush):
+                if(straight_begin && flush_idx)
+                {   
+                    if(must!=nullptr)
+                    {
+                        for(i=0;i<must->size();i++)
+                        {
+                            // cout<< P_NUM_INT(must->at(i)) <<"/"<<P_SUIT_INT(must->at(i)) <<endl;
+                            if(P_NUM_INT(must->at(i))  > straight_end)       
+                                break;
+                            if(P_NUM_INT(must->at(i)) < straight_begin && !(straight_end==14 && P_NUM_INT(must->at(i))==1))        
+                                break;
+                            if(P_SUIT_INT(must->at(i)) != flush_idx)
+                                break;
+                            if(i==must->size()-1)
+                                current = straight_flush;   
+                        }
+                    }
+                    else
+                        current = straight_flush;   
+                }
+            default:
+                return current;
+        }
     }   
-    bool is_straight_flush(int num,int bit) 
-    {
-        int cnt = 1;
-
- 
-        if(cnt == 5)
-            return true;
-        else
-            return false;       
-    }
-
 };
 int solve_uva_problem(std::istream &is,std::ostream &os)
 {
     Poker_131 player;
     while(1)
     {
+        
         if(!player.round_init(is,os))
             break;
         player.round_check();
         player.result_output(os);
     }
-        os<<std::endl;
     return 0;
 }
 
@@ -219,3 +365,18 @@ int main(int argc, char** argv)
     return RUN_ALL_TESTS();
 #endif    
 }
+#ifndef ONLINE_JUDGE
+TEST(uva_test, uva_test1)
+{
+    std::istringstream iss(
+        "TH JH QC QD QS QH KH AH 2S 6S\n"\
+         "2H 2S 3H 3S 3C 2D 3D 6C 9C TH\n"\
+        );
+    std::ostringstream oss;
+
+    solve_uva_problem(iss, oss);
+    EXPECT_EQ("Hand: TH JH QC QD QS Deck: QH KH AH 2S 6S Best hand: straight-flush\n"\
+    "Hand: 2H 2S 3H 3S 3C Deck: 2D 3D 6C 9C TH Best hand: four-of-a-kind\n"\
+    ,oss.str());
+}
+#endif
